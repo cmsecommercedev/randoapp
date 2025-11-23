910cabb4d71e355e7c1c0d0125917c405fb5e50c
Gokhan Gok <gokhan.gok@agx-labs.com>
2025-11-23 22:21:50 +0300

    dw

--- Managers/Interfaces/IEmployeeManager.cs
+++ Managers/Interfaces/IEmployeeManager.cs
@@ -3,4 +3,5 @@ public interface IEmployeeManager
     Task<EmployeeDto?> GetByIdAsync(Guid id);
     Task<EmployeeDto> CreateAsync(EmployeeCreateDto dto);
     Task<IEnumerable<TimeSpan>> GetAvailabilityAsync(Guid employeeId, DateTime date);
+    Task<IEnumerable<AppointmentDto>> GetAppointmentsAsync(Guid employeeId, DateTime date);
 }
